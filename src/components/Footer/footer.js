50ab6d25a8038fe90f6de5fa120add2dd10d80bc
Eduardo Rivero <lalo@lalorivero.com>
2021-10-26 10:24:33 -0600

    footer html structure

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,23 @@ import './footer.css'
 const Footer = () =>{
   return(
     <React.Fragment>
-      <div className="Footer"></div>
+      <div className="Footer">
+        <div className="Footer__docs-links">
+          <li className="docs-links">
+            <ol className="docs-links__element"></ol>
+            <ol className="docs-links__element"></ol>
+            <ol className="docs-links__element"></ol>
+          </li>
+        </div>
+        <div className="Footer__contact">
+          <li className="Footer__contact-list">
+            <ol className="List__element"></ol>
+            <ol className="List__element"></ol>
+            <ol className="List__element"></ol>
+            <ol className="List__element"></ol>
+          </li>
+        </div>
+      </div>
     </React.Fragment>
   )
 }
