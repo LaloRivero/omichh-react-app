9957010b1365ee796b005890012e7fd54e4f27f7
Eduardo Rivero <lalo@lalorivero.com>
2021-10-26 10:03:46 -0600

    Header logo structure

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-  return <h1> Header </h1>;
+  return (
+    <React.Fragment>
+      <div className="Header">
+        <div className="Header__logo">
+          <a href="/">
+            <img src="" alt="Logo" />
+          </a>
+        </div>
+        <div className="Header__menu"></div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Header;
