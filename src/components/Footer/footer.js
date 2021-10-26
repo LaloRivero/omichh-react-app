9d9b6b5f6bfa7cec768262eb31b5dc1091eab055
Eduardo Rivero <lalo@lalorivero.com>
2021-10-26 10:12:17 -0600

    Footer base structure

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () =>{
+  return(
+    <React.Fragment>
+      <div className="Footer"></div>
+    </React.Fragment>
+  )
+}
+
+export default Footer;
