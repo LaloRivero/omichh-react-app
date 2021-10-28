96156c10410743714fa2972ca15d69d50d94bda7
Eduardo Rivero <lalo@lalorivero.com>
2021-10-27 22:42:24 -0600

    Adding icons to the navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,17 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import * as FaIcons from "react-icons/fa"
+import React from "react";
+import { Link } from "react-router-dom";
+import {FaBars} from "react-icons/fa";
+import {AiOutlineClose} from 'react-icons/ai'
 
-const Navbar = () =>{
-  return(
+const Navbar = () => {
+  return (
     <React.Fragment>
       <div className="navbar">
-        <Link to='#' className='navbar__menu'>
-          <FaIcons.FaBars/>
+        <Link to="#" className="navbar__menu">
+          <FaBars />
         </Link>
       </div>
+      <nav className={sidebar ? "navbar__menu active" : "navnar__menu"}>
+        <ul className="navbar__menu-items">
+          <li className="navbar__toggle">
+            <Link className="menu__bars" to="#">
+              <AiOutlineClose/>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
