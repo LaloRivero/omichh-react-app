462a5cfb08466af6b36ff466e1a498e71a735b67
Eduardo Rivero <lalo@lalorivero.com>
2021-10-27 22:47:04 -0600

    setting the state of the side bar with useState

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import {FaBars} from "react-icons/fa";
-import {AiOutlineClose} from 'react-icons/ai'
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
   return (
     <React.Fragment>
       <div className="navbar">
         <Link to="#" className="navbar__menu">
-          <FaBars />
+          <FaBars onClick={showSidebar}/>
         </Link>
       </div>
       <nav className={sidebar ? "navbar__menu active" : "navnar__menu"}>
         <ul className="navbar__menu-items">
           <li className="navbar__toggle">
             <Link className="menu__bars" to="#">
-              <AiOutlineClose/>
+              <AiOutlineClose />
             </Link>
           </li>
         </ul>
