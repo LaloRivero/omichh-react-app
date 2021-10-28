4baaa03fb37ddd834fa183a50caffef35af0a88c
Eduardo Rivero <lalo@lalorivero.com>
2021-10-27 20:18:30 -0600

    Footer content structure

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -7,17 +7,50 @@ const Footer = () =>{
       <div className="Footer">
         <div className="Footer__docs-links">
           <li className="docs-links">
-            <ol className="docs-links__element"></ol>
-            <ol className="docs-links__element"></ol>
-            <ol className="docs-links__element"></ol>
+            <ol className="docs-links__element">
+              <a href="/">
+                <p className="elemt__text">CONVOCATORIA</p>
+              </a>
+            </ol>
+            <ol classNam="docs-links__element">
+              <a href="/">
+                <p className="elemt__text">REGLAMENTO</p>
+              </a>
+            </ol>
+            <ol className="docs-links__element">
+              <a href="/">
+                <p className="elemt__text">CODIGO DE CONDUCTA</p>
+              </a>
+            </ol>
+            <ol className="docs-links__element">
+                <a href="/">
+                  <p className="elemt__text">AVISO DE PRIVACIDAD</p>
+                </a>
+            </ol>
           </li>
         </div>
         <div className="Footer__contact">
           <li className="Footer__contact-list">
-            <ol className="List__element"></ol>
-            <ol className="List__element"></ol>
-            <ol className="List__element"></ol>
-            <ol className="List__element"></ol>
+            <ol className="List__element">
+              <a href="/">
+                <p className="elemt__text">contacto@omichh.org</p>
+              </a>
+            </ol>
+            <ol className="List__element">
+              <a href="/">
+                <img src="" alt="twitter" />
+              </a>
+            </ol>
+            <ol className="List__element">
+              <a href="/">
+                <img src="" alt="github" />
+              </a>
+            </ol>
+            <ol className="List__element">
+              <a href="/">
+                <img src="" alt="facebook" />
+              </a>
+            </ol>
           </li>
         </div>
       </div>
