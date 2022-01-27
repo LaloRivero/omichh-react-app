d0ce97197800a51e00247aa0ada8bb4fab0ad862
Eduardo Rivero <lalo@lalorivero.com>
2022-01-26 19:53:33 -0700

    Button component

---
+++ src/components/Button/button.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./button.css";
+
+const Button = (props) => {
+  return (
+  <React.Fragment>
+    <Link className="button">{props.layout}</Link>
+  </React.Fragment>
+  );
+};
+
+export default Button;
