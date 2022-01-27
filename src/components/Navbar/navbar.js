83160c6518cb3e315087c8a765b7f763ba5723e1
Eduardo Rivero <lalo@lalorivero.com>
2022-01-26 19:52:48 -0700

    Dark and white styles

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -3,28 +3,33 @@ import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 import { SidebarData } from "../SideBarData/sidebarData.js";
-import logowhite from "../../assets/logowhite.svg"
+import logowhite from "../../assets/logowhite.svg";
+import logoblue from "../../assets/logoblue.svg";
 import "./navbar.css";
 
-const Navbar = () => {
+const Navbar = (props) => {
   const [sidebar, setSidebar] = useState(false);
 
   const showSidebar = () => setSidebar(!sidebar);
 
   return (
     <React.Fragment>
-      <div className="navbar">
-        <Link to ="#" className="navbar__logo">
-          <img src={logowhite} alt="logo" />
+      <div className={props.theme === "light" ? "navbar navbar-light" : "navbar navbar-dark"}
+      >
+        <Link to="#" className="navbar__logo">
+          <img
+            src={props.theme === "light" ? logoblue : logowhite}
+            alt="logo"
+          />
         </Link>
-        <Link to="#" className="navbar__menu-bars">
+        <Link to="#" className={props.theme === "light" ? "navbar__menu-bars color-light" : "navbar__menu-bars color-dark"}>
           <FaBars onClick={showSidebar} />
         </Link>
       </div>
-      <nav className={sidebar ? "navbar__menu active" : "navbar__menu"}>
+      <nav className={sidebar ? "navbar__menu active navbar-dark" : "navbar__menu"}>
         <ul className="navbar__menu-items" onClick={showSidebar}>
           <li className="navbar__toggle">
-            <Link className="menu__bars-close" to="#">
+            <Link className="menu__bars-close color-dark" to="#">
               <AiOutlineClose />
             </Link>
           </li>
