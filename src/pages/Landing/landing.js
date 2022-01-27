78e08e2006417a01f608eee52180aedd8c2f04f5
Eduardo Rivero <lalo@lalorivero.com>
2022-01-26 21:01:25 -0700

    langing page

--- src/pages/Landing/landing.js
+++ src/pages/Landing/landing.js
@@ -1,24 +1,32 @@
 import React from "react";
 import Navbar from "../../components/Navbar/navbar";
-import medals from "../../assets/medallas.png"
-import Button from "../../components/Button/button"
+import medals from "../../assets/medallas.png";
+import Button from "../../components/Button/button";
 import "./landing.css";
 
 class Landing extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar theme="dark"/>
-        <div className="landing__container">
-          <div className="landing__content">
-            <h1 className="content__title">Buscamos a los mejores programadores de Chihuahua.</h1>
-            <p className="content__text">
-            La OMICHH es un concurso en el que sobre todo se requiere tener <strong>facilidad, habilidad y voluntad de resolver problemas</strong> utilizando la lógica, el ingenio y las computadoras.
-            </p>
-            <Button layout="Participa" theme="dark"/>
-          </div>
-          <div className="landing__medals">
-            <img src={medals} alt="Medals" className="medals__img" />
+        <div className="landing">
+          <Navbar theme="dark" />
+          <div className="landing__container">
+            <div className="landing__content">
+              <h2 className="content__title">
+                Buscamos a los mejores programadores de Chihuahua.
+              </h2>
+              <p className="content__text">
+                La OMICHH es un concurso en el que sobre todo se requiere tener{" "}
+                <strong>
+                  facilidad, habilidad y voluntad de resolver problemas
+                </strong>{" "}
+                utilizando la lógica, el ingenio y las computadoras.
+              </p>
+              <Button layout="Participa" theme="dark" />
+            </div>
+            <div className="landing__medals">
+              <img src={medals} alt="Medals" className="medals__img" />
+            </div>
           </div>
         </div>
       </React.Fragment>
