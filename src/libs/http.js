a47e6791a15b587f7b4d7bfa2842a4c4af5f971a
Eduardo Rivero <lalo@lalorivero.com>
2022-01-27 19:32:35 -0700

    REST library

---
+++ src/libs/http.js
@@ -0,0 +1,63 @@
+import URL from "./url.js";
+
+class Http {
+  static instance = new Http();
+
+  get_participants = async () => {
+    try {
+      const request = await fetch(`${URL.omichh_api}/participants/`);
+      const response = await request.json();
+      return response;
+    } catch (error) {
+      throw Error(error);
+    }
+  };
+
+  get_schools = async () => {
+    try {
+      const request = await fetch(`${URL.omichh_api}/schools/`);
+      const response = await request.json();
+      return response;
+    } catch (error) {
+      throw Error(error);
+    }
+  };
+
+  get_school = async (id) => {
+    try {
+      const request = await fetch(`${URL.omichh_api}/schools/${id}/`);
+      const response = await request.json();
+      return response;
+    } catch (error) {
+      throw Error(error);
+    }
+  };
+
+  add_participant = async (participant) => {
+    try {
+      const request = await fetch(`${URL.omichh_api}/participants/`, {
+        method: "POST",
+        body: JSON.stringify(participant),
+      });
+      const response = await request.json();
+      return response;
+    } catch (error) {
+      throw Error(error);
+    }
+  };
+
+  add_school = async (school) => {
+    try {
+      const request = await fetch(`${URL.omichh_api}/schools/`, {
+        method: "POST",
+        body: JSON.stringify(school),
+      });
+      const response = await request.json();
+      return response;
+    } catch (error) {
+      throw Error(error);
+    }
+  };
+}
+
+export default Http;
---
+++ src/libs/url.js
@@ -0,0 +1,4 @@
+let URL = {
+  omichh_api: "http:127.0.0.1:8000",
+};
+export default URL;
