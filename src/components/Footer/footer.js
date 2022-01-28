b83323b83b968736b624e81aa97d3891354831fd
Eduardo Rivero <lalo@lalorivero.com>
2022-01-27 20:10:42 -0700

    Footer light mode

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,32 +1,67 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import facebook from '../../assets/facebook.svg'
-import './footer.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import w_facebook from "../../assets/wfacebook.svg";
+import b_facebook from "../../assets/bfacebook.svg";
+import "./footer.css";
 
-const Footer = (props) =>{
-  return(
+const Footer = (props) => {
+  return (
     <React.Fragment>
-      <div className="Footer">
+      <div
+        className={
+          props.theme === "light"
+            ? "Footer b-color-light"
+            : "Footer b-color-dark"
+        }
+      >
         <div className="Footer__docs-links">
           <li className="docs-links">
             <ol className="links__element">
-                <Link className="element__link" to="/">AVISO DE PRIVACIDAD</Link>
+              <Link
+                className={
+                  props.theme === "light"
+                    ? "element__link color-light"
+                    : "element__link color-dark"
+                }
+                to="/"
+              >
+                AVISO DE PRIVACIDAD
+              </Link>
             </ol>
           </li>
         </div>
         <div className="Footer__contact">
           <li className="Footer__contact-list">
             <ol className="list__element">
-              <Link className="element__link" to="/">contacto@omichh.org</Link>
+              <Link
+                className={
+                  props.theme === "light"
+                    ? "element__link color-light"
+                    : "element__link color-dark"
+                }
+                to="/"
+              >
+                contacto@omichh.org
+              </Link>
             </ol>
             <ol className="list__element">
-              <a href="https://www.facebook.com/OMI.Chihuahua" rel="noopener noreferrer" target="_blank"><img className="Facebook__icon" src={facebook} alt="facebook" /></a>
+              <a
+                href="https://www.facebook.com/OMI.Chihuahua"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <img
+                  className="Facebook__icon"
+                  src={props.theme === "light" ? b_facebook : w_facebook}
+                  alt="facebook"
+                />
+              </a>
             </ol>
           </li>
         </div>
       </div>
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default Footer;
