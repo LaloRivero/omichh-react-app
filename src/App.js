7aa2674fca3602b9d6595a1474a37a383c012425
Eduardo Rivero <lalo@lalorivero.com>
2022-01-27 20:10:12 -0700

    Adding participants path

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
+import "./App.css";
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Landing from './pages/Landing/landing';
-import About from './pages/About/about';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Landing from "./pages/Landing/landing";
+import About from "./pages/About/about";
+import Participants from "./pages/Participants/participants";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path='/' component={Landing}/>
-        <Route exact path='/about/' component={About}/>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/participants" component={Participants} />
       </Switch>
     </Router>
   );
