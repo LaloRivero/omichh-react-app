958dde0c0945dc631fd69777efbc7d697c5998b8
Eduardo Rivero <lalo@lalorivero.com>
2022-01-27 20:11:13 -0700

    Adding all participants page

--- src/components/SideBarData/sidebarData.js
+++ src/components/SideBarData/sidebarData.js
@@ -5,12 +5,19 @@ import { RiPencilRulerLine } from "react-icons/ri";
 import { HiOutlineDocumentText } from "react-icons/hi";
 import { VscNotebook } from "react-icons/vsc";
 import { MdPermContactCalendar } from "react-icons/md";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 export const SidebarData = [
   {
     title: "Registro",
     path: "/",
     icon: <FaRegIdBadge />,
+    cName: "navbar__text",
+  },
+  {
+    title: "Alumnos Inscritos",
+    path: "/participants",
+    icon: <BsFillPersonLinesFill />,
     cName: "navbar__text",
   },
   {
