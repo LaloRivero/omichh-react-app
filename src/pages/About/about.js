0bf9648742c3418a079302ac440f1185c8994f7c
Eduardo Rivero <lalo@lalorivero.com>
2022-01-27 19:00:46 -0700

    About page

---
+++ src/pages/About/about.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Navbar from "../../components/Navbar/navbar";
+import Footer from "../../components/Footer/footer";
+import Programing_img from "../../assets/Programming.png"
+import "./about.css";
+
+const About = () => {
+  return (
+    <React.Fragment>
+      <div className="about">
+        <Navbar theme="dark" />
+        <div className="about__container">
+          <div className="about__content-text">
+            <h1>Olimpiada Mexicana de Informática</h1>
+            <p>
+              La Olimpiada Mexicana de Informática en Chihuahua (OMICHH) es un
+              concurso a nivel estatal para jóvenes y niños con facilidad para
+              resolver problemas prácticos mediante la lógica y el uso de
+              computadoras, que busca promover el desarrollo tecnológico en
+              México y encontrar a los mejores programadores, quienes formarán
+              la selección del estado para participar en las próximas Olimpiadas
+              Nacionales de Informática (OMI) y posteriormente en Olimpiadas
+              Internacionales de Informática (IOI).
+            </p>
+            <p>
+              <i>
+                La OMICHH es un concurso en el que sobre todo se requiere tener
+                facilidad, habilidad y voluntad de resolver problemas utilizando
+                la lógica, el ingenio y las computadoras.
+              </i>
+            </p>
+          </div>
+          <div className="about__content-img">
+            <img src={Programing_img} alt="coding" />
+          </div>
+        </div>
+        <Footer theme="dark" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default About;
