b40305d16f500d4bef4cc0fc76034d5730cf2c23
Eduardo Rivero <lalo@lalorivero.com>
2022-01-30 21:09:02 -0700

    Filtering the participants data to show it in different tables

--- src/pages/Participants/participants.js
+++ src/pages/Participants/participants.js
@@ -1,24 +1,44 @@
 import React from "react";
 import Navbar from "../../components/Navbar/navbar";
 import Footer from "../../components/Footer/footer";
+import ParticipantTable from "../../components/ParticipantTable/participantTable";
+import team from "../../assets/Team-amico.svg";
+import Http from "../../libs/http";
 import "./participants.css";
-
 class Participants extends React.Component {
   state = {
     theme: "light",
+    omi: null,
+    omis: null,
+    omip: null,
   };
+  componentDidMount() {
+    this.getParticipants();
+  }
+
+  getParticipants = async () => {
+    let response = await Http.instance.get_participants();
+    const data = response.results
+    console.log(data)
+    this.setState({ omi: data.filter((participant)=>participant.category==='OMI'),
+                    omis: data.filter((participant)=>participant.category==='OMIS'),
+                    omip: data.filter((participant)=>participant.category==='OMIP'),});
+  };
+
   render() {
     return (
       <React.Fragment>
         <div className="participants">
           <Navbar theme={this.state.theme} />
           <div className="participants__hero">
-            <img src="" alt="" className="hero__img" />
+            <img className="hero__img" src={team} alt="team" />
             <div className="hero__search">
-              <input type="text" />
+              <input type="text" placeholder="Buscar participante" />
             </div>
           </div>
-          <h1>Participants</h1>
+          {this.state.omi ?<ParticipantTable data={this.state.omi} category={"OMI"}/>: null}
+          {this.state.omis ?<ParticipantTable data={this.state.omis} category={"OMIS"}/>: null}
+          {this.state.omip ?<ParticipantTable data={this.state.omip} category={"OMIP"}/>: null}
           <Footer theme={this.state.theme} />
         </div>
       </React.Fragment>
