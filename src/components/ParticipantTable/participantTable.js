e1c3d20e01d03a5ce1a90925e5902e0dbb268864
Eduardo Rivero <eduardo.rivero@cobachih.edu.mx>
2022-03-23 16:49:51 -0700

    participants table

--- src/components/ParticipantTable/participantTable.js
+++ src/components/ParticipantTable/participantTable.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import DataTable from "react-data-table-component";
 import "./participantTable.css";
+import search_icon from "../../assets/search.png";
 
 const ParticipantTable = (props) => {
+  const [search, setSearch] = useState("");
+
+  const filteredParticipants = props.data.filter((participant) => {
+    return (
+      participant.first_name.toLowerCase().includes(search.toLowerCase()) ||
+      participant.last_name.toLowerCase().includes(search.toLowerCase())
+    );
+  });
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
   const columns = [
     {
       name: "Nombre",
@@ -15,34 +29,41 @@ const ParticipantTable = (props) => {
       sortable: true,
     },
     {
-      name: "Categoría",
-      selector: (row) => row.category,
-      sortable: true,
-    },
-    {
       name: "Escuela",
       selector: (row) => row.school.name,
       sortable: true,
     },
     {
       name: "Verificado",
-      selector : (row) => {
-        if(row.is_verified){
-          return "✔"
-        }else{
-          return "❌"
-        }},
-      sortable:true,
-    }
+      selector: (row) => {
+        if (row.is_verified) {
+          return "✔";
+        } else {
+          return "❌";
+        }
+      },
+      sortable: true,
+    },
   ];
   return (
     <React.Fragment>
       <div className="dataTable__container">
-        <h3>{"Participantes " + props.category}</h3>
+        <div className="dataTable__tableTitle">
+          <h3>{"Participantes " + props.category}</h3>
+          <div className="search__input">
+            <img className="search__icon" src={search_icon} alt="search" />
+            <input
+              className="input"
+              value={search}
+              onChange={handleSearch}
+              type="text"
+            />
+          </div>
+        </div>
         <div className="dataTable__content">
           <DataTable
             columns={columns}
-            data={props.data}
+            data={filteredParticipants}
             highlightOnHover={true}
             noDataComponent={"No hay alumnos inscritos"}
           />
