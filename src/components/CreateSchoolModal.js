25893100c45bc6e402d14c590f7cd5a323ab82e3
Lalo Rivero <eduardo.rivero@metergroup.com>
2022-04-16 11:56:46 -0600

    styled elements in modal component

--- src/components/CreateSchoolModal.js
+++ src/components/CreateSchoolModal.js
@@ -1,6 +1,57 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useRef, useEffect, useCallback } from "react";
+import { useSpring, animated } from "react-spring";
+import styled from "styled-components";
+import { MdClose } from 'react-icons/md';
 
-export const Modal = () =>{
-    
-}
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.2);
+  position: fixed;
+  display: fixed;
+  justify-content: center;
+  align-items: center;
+`;
+const ModalWrapper = styled.div`
+  width: 800px;
+  height: 500px;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #000;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  position: relative;
+  z-index: 10;
+  border-radius: 10px;
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.8;
+  color: #141414;
+  p {
+    margin-bottom: 1rem;
+  }
+  button {
+    padding: 10px 24px;
+    background: #141414;
+    color: #fff;
+    border: none;
+  }
+`;
+
+const CloseModalButton = styled(MdClose)`
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 32px;
+  height: 32px;
+  padding: 0;
+  z-index: 10;
+`;
+
+export const Modal = ({ showModal, handleToggleModal }) => {};
