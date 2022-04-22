c4f416ae0a8d2e3405af05d29d6b9411df6a35f3
Lalo Rivero <eduardo.rivero@metergroup.com>
2022-04-21 20:05:09 -0600

    loader component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="Loader__container">
+      <div className="loader" />
+    </div>
+  );
+};
+
+export default Loader;
