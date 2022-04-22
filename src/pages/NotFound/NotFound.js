4d86829429667a8a86c2d1cf1ad60a6ba6a74eac
Lalo Rivero <eduardo.rivero@metergroup.com>
2022-04-22 12:21:37 -0600

    not found page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import Navbar from "../../components/Navbar/navbar";
+import Footer from "../../components/Footer/footer";
+import error404 from "../../assets/404Error.png";
+import "./NotFound.css";
 
 const Notfound = () => {
-    return (
-        <div>
-            Error
+  return (
+    <React.Fragment>
+      <div className="participants">
+        <Navbar theme={"light"} />
+        <div className="error_404__container">
+          <img src={error404} alt="not found" />
         </div>
-    );
-}
+        <Footer theme={"light"} />
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Notfound;
