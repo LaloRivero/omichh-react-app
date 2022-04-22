91a79e745c56cdd3c9b3619504f797280e5f8abb
Lalo Rivero <eduardo.rivero@metergroup.com>
2022-04-22 14:51:41 -0600

    Adding verify token page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./pages/About/about";
 import Participants from "./pages/Participants/participants";
 import Register from "./pages/Register/register";
 import NotFound from "./pages/NotFound/NotFound";
+import VerifyAccount from "./pages/VerifyAccount/VerifyAccount";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/about" component={About} />
         <Route exact path="/participants" component={Participants} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/verify/:token" component={VerifyAccount}/>
         <Route component={NotFound}></Route>
       </Switch>
     </Router>
