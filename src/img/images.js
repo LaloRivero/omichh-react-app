4dab31b55011360620e1a1f0b5f426d50ae4b3ab
Lalo Rivero <eduardo.rivero@metergroup.com>
2022-04-23 12:30:54 -0600

    image urls library

---
+++ src/img/images.js
@@ -0,0 +1,20 @@
+export const logo_blue =
+  "https://res.cloudinary.com/dwwj1fhcd/image/upload/v1650735703/logo_blue_hjv8zw.svg";
+export const logo_white =
+  "https://res.cloudinary.com/dwwj1fhcd/image/upload/v1650735703/logo_white_lw1enm.svg";
+export const medals =
+  "https://res.cloudinary.com/dwwj1fhcd/image/upload/v1650735703/medallas_cynmh7.png";
+export const b_facebook =
+  "https://res.cloudinary.com/dwwj1fhcd/image/upload/v1650735703/bfacebook_glvjbw.svg";
+export const w_facebook =
+  "https://res.cloudinary.com/dwwj1fhcd/image/upload/v1650735712/wfacebook_sjdmwq.svg";
+export const error404 =
+  "https://res.cloudinary.com/dwwj1fhcd/image/upload/v1650735703/404Error_em66ol.png";
+export const ok =
+  "https://res.cloudinary.com/dwwj1fhcd/image/upload/v1650735704/ok_awthnh.png";
+export const no_data =
+  "https://res.cloudinary.com/dwwj1fhcd/image/upload/v1650735704/no_data_eabyra.png";
+export const team =
+  "https://res.cloudinary.com/dwwj1fhcd/image/upload/v1650735712/Team-amico_tcu26w.svg";
+export const binary =
+  "https://res.cloudinary.com/dwwj1fhcd/image/upload/v1650735703/binary_umujpu.png";
