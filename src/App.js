97a520a5b8737a68d81c7f07bd5f01a18e8d954b
Lalo Rivero <eduardo.rivero@metergroup.com>
2023-02-25 12:32:09 -0600

    fix: enable routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Landing from "./pages/Landing/landing";
 import About from "./pages/About/about";
 // import Participants from "./pages/Participants/participants";
-// import Register from "./pages/Register/register";
+import Documents from "./pages/Documents/Documents";
+import Register from "./pages/Register/register";
 import NotFound from "./pages/NotFound/NotFound";
 // import VerifyAccount from "./pages/VerifyAccount/VerifyAccount";
 import "./App.css";
@@ -15,9 +16,10 @@ function App() {
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route exact path="/about" component={About} />
-        {/* <Route exact path="/participants" component={Participants} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/verify/:token" component={VerifyAccount}/> */}
+        {/* <Route exact path="/participants" component={Participants} /> */}
+        <Route exact path="/registro" component={Register} />
+        <Route exact path="/convocatoria" component={Documents} />
+        {/* <Route exact path="/verify/:token" component={VerifyAccount}/> */}
         <Route component={NotFound}></Route>
       </Switch>
     </Router>
