cd449d9cfa574beda6b0b2bd7d1a4287963f2e9e
Lalo Rivero <eduardo.rivero@metergroup.com>
2023-02-25 12:30:47 -0600

    fix: delete unnecessary code

--- src/pages/Register/register.js
+++ src/pages/Register/register.js
@@ -4,6 +4,7 @@ import Navbar from "../../components/Navbar/navbar";
 import Footer from "../../components/Footer/footer";
 import "./register.css";
 
+const srcFormURL = "https://docs.google.com/forms/d/e/1FAIpQLSfdP0E-6479qC9Q5q243TwiPogg3DPEXYjz6DnnxyckwZ2TiQ/viewform?embedded=true"
 class Register extends React.Component {
   render() {
     return (
@@ -11,8 +12,15 @@ class Register extends React.Component {
         <div className="register">
           <Navbar theme="dark"/>
           <div className="register__content">
-            <h1 className="register__title">Register</h1>
-            <RegisterForm />
+            <h1 className="register__title">Registro</h1>
+            <iframe
+              src={srcFormURL}
+              width="640"
+              height="1720"
+              frameborder="0"
+              marginheight="0"
+              marginwidth="0"
+            />
           </div>
           <Footer theme="dark"/>
         </div>
