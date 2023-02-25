77b060f13d2737ee0870ddab98d1ee21be7efb15
Lalo Rivero <eduardo.rivero@metergroup.com>
2023-02-25 12:31:08 -0600

    chore: add md strings

---
+++ src/static/omi.jsx
@@ -0,0 +1,76 @@
+export const omi = `# **Olimpiada Mexicana de Informática Chihuahua**
+
+El Gobierno de Chihuahua a través de la Secretaría de Educación y Deporte y la Olimpiada Mexicana de Informática Chihuahua (OMICHH).
+
+## **CONVOCAN**
+
+A todos los alumnos que radiquen en el Estado de Chihuahua a participar en la 28° Olimpiada Mexicana de Informática en su fase Estatal, la cual permitirá seleccionar a los alumnos más destacados para su participación en la OMI Nacional 2023 a celebrarse la primera quincena del mes de Octubre de 2023.
+
+La **Olimpiada Mexicana de Informática en Chihuahua (OMICHH)** es un concurso a nivel estatal para jóvenes y niños con facilidad para resolver problemas prácticos mediante la lógica y el uso de computadoras, que busca promover el desarrollo tecnológico en México y encontrar a los mejores programadores, quienes formarán la selección del estado para participar en las próximas Olimpiadas Nacionales de Informática (OMI) y posteriormente en Olimpiadas Internacionales de Informática (IOI).
+
+La OMICHH es un concurso en el que sobre todo se requiere tener facilidad, habilidad y voluntad de resolver problemas utilizando la lógica, el ingenio y las computadoras.
+
+## **La misión de la OMI y la OMICHH es:**
+
+Promover el interés por la informática y las ciencias de la computación entre los estudiantes del país.
+Fomentar el desarrollo tecnológico en México.
+Encontrar a jóvenes talentosos para brindarles apoyo en sus estudios y guía para sus inquietudes.
+Promover la amistad, convivencia e intercambio tecnológico entre jóvenes de todo el país con intereses comunes en la programación.
+
+### **I. Requisitos y Condiciones.**
+
+Haber nacido después del 1° de julio de 2004.
+Tener nacionalidad mexicana o radicar en México y estar dispuesto a representar internacionalmente a México.
+Estar inscrito en una institución de nivel básico o medio superior con una
+carga curricular completa en el sistema educativo mexicano.
+Firmar de conformidad el reglamento y código de conducta por parte del alumno y padre o tutor, el cual será enviado una vez que se realice el correspondiente registro.
+Estar cursando un grado tal que para el semestre de enero a junio del 2024 aún se encuentre en nivel básico o medio superior.
+
+
+Los conocimientos y habilidades mínimas con las que deberán contar son las siguientes: Matemáticas básicas: Aritmética, Álgebra y Trigonometría. Gusto y placer por resolver problemas y retos.
+
+### **II.  Inscripciones**
+Las inscripciones iniciarán el 18 de febrero de 2023 y finalizarán el 31 de agosto de 2023.
+Los concursantes deben registrarse en la siguiente liga. www.omichh.org
+Asesores si desean recibir más información, favor de enviar un correo electrónico a contacto@omichh.org.
+
+Para más información visita nuestra página oficial www.omichh.org o cualquier duda envía un correo electrónico a contacto@omichh.org.
+
+
+### **Desarrollo de la OMICHH**
+
+El proceso Estatal de la OMICHH se desarrolla en las etapas y fechas a continuación descritas:
+
+Categoría Abierta
+* Inscripciones.
+* Desarrollo del curso estatal en omegaup, al cual serán agregados una vez se registren.
+* Examen Pre nacional se llevará a cabo de manera presencial en la ciudad de Chihuahua, Chih. Durante el mes de septiembre de 2023.
+* El sábado 2 de septiembre de 2023 a las 10:00 hrs a 14:30 hrs. Se efectuará la primera fase del examen estatal de manera virtual.
+* El sábado 9 de septiembre de 2023 a las 10:00 hrs a 14:30 hrs. Se efectuará la segunda fase del examen estatal de manera presencial, la sede del examen se informará próximo a la fecha.
+* El examen Pre-Nacional será el que determinará a los 4 alumnos que integraran a la delegación nacional que representará a Chihuahua en la OMI Nacional a celebrarse en el mes de octubre de 2023.
+
+
+**Temario para media superior:**
+
+	Bases del lenguaje de programación C++:
+    - Variables
+    - Tipos de datos
+    - Sentencia IF
+    - Ciclos While & For
+    - Funciones y procedimientos
+
+    Estructuras de datos:
+    - Arreglos
+    - Matrices
+    - Pilas, Colas
+    - Listas
+    - Árboles
+
+	Algoritmos:
+    - Búsqueda binaria
+    - Barridos
+    - Cubetaje
+    - Greedy
+    - Recursividad
+    - Recorridos y búsquedas en grafos (Profundidad, anchura y en primera prioridad)
+`
---
+++ src/static/omips.jsx
@@ -0,0 +1,91 @@
+export const omips = `# **Olimpiada Mexicana de Informática Chihuahua**
+
+El Gobierno de Chihuahua a través de la Secretaría de Educación y Deporte y la Olimpiada Mexicana de Informática Chihuahua (OMICHH).
+
+## **CONVOCAN**
+
+A todos los alumnos que radiquen en el Estado de Chihuahua a participar en la 28° Olimpiada Mexicana de Informática en su fase Estatal, la cual permitirá seleccionar a los alumnos más destacados para su participación en la OMI Nacional 2023 a celebrarse la primera quincena del mes de Octubre de 2023.
+
+La **Olimpiada Mexicana de Informática en Chihuahua (OMICHH)** es un concurso a nivel estatal para jóvenes y niños con facilidad para resolver problemas prácticos mediante la lógica y el uso de computadoras, que busca promover el desarrollo tecnológico en México y encontrar a los mejores programadores, quienes formarán la selección del estado para participar en las próximas Olimpiadas Nacionales de Informática (OMI) y posteriormente en Olimpiadas Internacionales de Informática (IOI).
+
+La OMICHH es un concurso en el que sobre todo se requiere tener facilidad, habilidad y voluntad de resolver problemas utilizando la lógica, el ingenio y las computadoras.
+
+## **La misión de la OMI y la OMICHH es:**
+
+Promover el interés por la informática y las ciencias de la computación entre los estudiantes del país.
+Fomentar el desarrollo tecnológico en México.
+Encontrar a jóvenes talentosos para brindarles apoyo en sus estudios y guía para sus inquietudes.
+Promover la amistad, convivencia e intercambio tecnológico entre jóvenes de todo el país con intereses comunes en la programación.
+
+**La OMIPS se divide en 2 categorías**
+
+* Primaria
+* Secundaria
+
+## **La OMIPS consiste en las siguientes etapas:**
+
+**Etapa estatal:** En esta etapa los participantes deberán presentar una serie de exámenes para seleccionar a los mejores programadores de Chihuahua a nivel educación básica.
+Examen nacional en línea: En esta etapa los representantes de todos los estados del país presentarán en simultáneo un examen a nivel nacional. Los que obtengan los mejores resultados serán seleccionados para la etapa final.
+
+**Etapa final:** Los 15 mejores alumnos de cada categoría serán invitados a participar en la etapa nacional de la Olimpiada Mexicana de Informática OMI que se llevará a cabo durante el mes de octubre del 2023.
+Participación en la etapa de Preselección: Aquellos alumnos que obtengan una medalla de oro en la etapa final (mejores 5 puntajes) serán invitados a participar en el proceso de entrenamiento y selección para la Olimpiada Internacional de Informática.
+
+### **I. Requisitos y Condiciones.**
+
+Estar inscrito en cualquier institución educativa del Estado en los niveles de Primaria o Secundaria.
+Firmar de conformidad el reglamento y código de conducta por parte del alumno y padre o tutor, el cual será enviado una vez que se realice el correspondiente registro.
+Para la categoría OMI Secundarias estar cursando como máximo el tercer año de educación secundaria y para la categoría OMI Primarias estar cursando como máximo el sexto año de educación primaria.
+Los conocimientos y habilidades mínimas con las que deberán contar son las siguientes: Matemáticas básicas: Aritmética, Álgebra y Trigonometría. Gusto y placer por resolver problemas y retos.
+
+## **II.  Inscripciones**
+Las inscripciones iniciarán el 18 de febrero de 2022 y finalizarán el 30 de abril de 2023.
+Los concursantes deben registrarse en la siguiente liga. www.omichh.org
+Asesores si desean recibir más información, favor de enviar un correo electrónico a contacto@omichh.org.
+
+Para más información visita nuestra página oficial www.omichh.org o cualquier duda envía un correo electrónico a contacto@omichh.org.
+
+## **III. Examen nacional en línea**
+El examen nacional en línea se llevará a cabo el sábado 27 de mayo de 2023 a las 10:30am hora central de México y tendrá una duración de 5 horas.
+
+En el examen nacional en línea solamente pueden participar los alumnos que hayan obtenido su pase al examen y hayan sido registrados correctamente. Los concursantes presentarán el examen en alguna de las sedes que el delegado estatal designe.
+
+Cada sede estará supervisada por alguien aprobado por el comité estatal.
+
+## **Desarrollo de la OMICHH**
+
+### **Categoría Primarias y Secundarias**
+
+* Inscripciones (Febrero, Marzo y Abril).
+* Del sábado 22 de abril al 28 de abril de 2022. Se abrirá una práctica en línea para cada nivel, el realizar la práctica es opcional y tiene la intención de mostrar a los alumnos el uso de omegaup y los concursos.
+* El sábado 6 de mayo de 2023 a las 10:00 hrs a 13:30 hrs. Se efectuará el primer examen estatal de manera virtual.
+* El sábado 13 de mayo de 2023 a las 10:00 hrs a 13:30 hrs. Se efectuará el primer examen estatal de manera presencial, la sede del examen se informará próximo a la fecha. 
+* Ambos exámenes sumarán puntos para determinar a los alumnos que pasaran a la siguiente etapa.
+Este examen determinará a los 25 alumnos que pasarán a la etapa nacional, es necesario que los alumnos que sean parte de este grupo hayan conseguido un puntaje diferente a cero durante el proceso estatal.
+* Los concursos tendrá 3 problemas para primaria y 3 para secundaria, con una duración de 3:30 horas para ambos niveles. Todos los problemas deberán ser resueltos utilizando el lenguaje de programación ​Karel​, utilizando el sistema de evaluación ​OmegaUp.
+* El sábado 27 de mayo de 2023, Se abrirá un concurso nacional en línea para cada nivel. El concurso tendrá 3 problemas para primaria y 3 para secundaria, con una duración de 4 horas para ambos niveles. Todos los problemas deberán ser resueltos usando el lenguaje de programación ​Karel​, empleando el sistema de evaluación ​OmegaUp.
+
+
+### **El temario para primaria será:**
+
+* Uso de instrucciones básicas (​avanza​, ​gira-izquierda​, ​coge-zumbador​,deja-zumbador​, ​apágate​)
+* Uso de bifurcaciones condicionales (​si ___ entonces ____ sino​)
+* Uso de ciclos (​repetir​, ​mientras​)
+* Uso de condiciones para controlar el flujo, (​frente-libre​, ​orientado-al-norte​, junto-a-zumbador​, etc.)
+* Uso de las instrucciones ​sucede ​y ​precede
+* Definición de instrucciones con parámetros
+* Recursividad únicamente con el objetivo de incrementar o decrementar una variable.
+* Recursividad para contar, comparar cantidades o memorizar números.
+
+
+### **El temario para secundaria será:**
+
+* Uso de instrucciones básicas (​avanza​, ​gira-izquierda​, ​coge-zumbador​, deja-zumbador​, ​apágate​)
+* Uso de bifurcaciones condicionales (​si ___ entonces ____ sino​)
+* Uso de ciclos (​repetir​, ​mientras​)
+* Uso de condiciones para controlar el flujo, (​frente-libre​, ​orientado-al-norte​, junto-a-zumbador​, etc.)
+* Uso de las instrucciones ​sucede ​y ​precede
+* Definición de instrucciones con parámetros
+* Recursividad únicamente con el objetivo de incrementar o decrementar una variable.
+* Recursividad para contar, comparar cantidades o memorizar números.
+* Búsquedas
+`
